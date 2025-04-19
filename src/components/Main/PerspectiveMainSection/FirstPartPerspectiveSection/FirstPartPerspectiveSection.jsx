0012dd8c327ab61ef7f59892d8320621cdcb4f67
Hermine2002 <herminesevumyan850@gmail.com>
2025-04-19 14:24:26 +0400

    changes

--- src/components/Main/PerspectiveMainSection/FirstPartPerspectiveSection/FirstPartPerspectiveSection.jsx
+++ src/components/Main/PerspectiveMainSection/FirstPartPerspectiveSection/FirstPartPerspectiveSection.jsx
@@ -1,16 +1,19 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import "./FirstPartPerspectiveSection.css";
 
 const FirstPartPerspectiveSection = () => {
   return (
-    <section className="ourPers">
+    <section className="ourpers">
+      <div className="fone-perspective">     
       <h2 className="main_text">
         Our Perspective
         <br />
-        <span className="home">HOME </span>
-        <span> | </span>
-        <span className="pers"> OUR PERSPECTIVE</span>
+        <Link className="home">HOME </Link>
+        <span className="ss"> | </span>
+        <Link className="pers"> OUR PERSPECTIVE</Link>
       </h2>
       <div className="fakeShadow"></div>
+      </div>
     </section>
   );
 };
--- src/components/Main/PerspectiveMainSection/SecondPartPerspectiveSection/SecondPartPerspectveSection.jsx
+++ src/components/Main/PerspectiveMainSection/SecondPartPerspectiveSection/SecondPartPerspectveSection.jsx
@@ -1,155 +1,32 @@
-import React from "react";
+import perspectivephoto1 from "../../../../assets/imgs/5TQUCyH6qAyEHyjd31J3BIEEyBgpgXzwBvcUUsJw.png"
+import perspectivephoto2 from "../../../../assets/imgs/OtkldinvaxOaXIUTfXcSkyu5hPFatPnu9NMtKvFh.jpg"
+import perspectivephoto3 from "../../../../assets/imgs/FnGfSsgABe4ruPpsoSZJBrfdwGO6SVgTNglLdWRV.jpg"
+
 import "./SecondPartPerspective.css";
 
 const SecondPartPerspectveSection = () => {
   return (
     <section className="perspective_section">
-      <div className="top">
-        <div className="cub_2">
-          <span className=""></span>
-          <span className=""></span>
-          <span className=""></span>
-          <span className=""></span>
-          <span className=""></span>
-          <span className=""></span>
-          <span className=""></span>
-          <span className=""></span>
-          <span className=""></span>
-          <span className=""></span>
-          <span className=""></span>
-          <span className=""></span>
-          <span className=""></span>
-          <span className=""></span>
-          <span className=""></span>
-          <span className=""></span>
-          <span className=""></span>
-          <span className=""></span>
-          <span className=""></span>
-          <span className=""></span>
-          <span className=""></span>
-          <span className=""></span>
-          <span className=""></span>
-          <span className=""></span>
-          <span className="active"></span>
-          <span className=""></span>
-          <span className=""></span>
-          <span className=""></span>
-          <span className=""></span>
-          <span className=""></span>
-          <span className=""></span>
-          <span className=""></span>
-          <span className=""></span>
-          <span className=""></span>
-          <span className=""></span>
-          <span className=""></span>
-          <span className=""></span>
-          <span className=""></span>
-          <span className=""></span>
-          <span className=""></span>
-          <span className=""></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
+      <div className="kanteiner-perpective">
+        <div className="text-photo">
+          <div className="kanteiner-textboxx">
+            <h3 className="kanteiner-textboxx-h3">About Our Agency</h3>
+            <h2 className="kanteiner-textboxx-h2">Your trusted partner <br />
+            for business</h2>
+            <p className="kanteiner-textboxx-p">this oois the main factor that sets us apart from our competition and <br />
+            allows us to deliver a specialist business consultancy service. Our team <br />
+            applies its wide-ranging experience to determining. Through our years <br />
+            of experience, we’ve also learned that while each channel.</p>
+          </div>
+          <div className="kanteiner-textbox">
+            <img src={perspectivephoto1} alt="perspectivephoto1" className="perspectivephoto1"/>
+          </div>
 
-        <h1>What perspective can you get with us </h1>
-      </div>
-      <div className="inner">
-        <div className="middle">
-          <p className="middle_p">Lorem ipsum dolor </p>
-          <h2 className="middle_h2">
-            Lorem ipsum dolor
-            <br /> sit amet{" "}
-          </h2>
-          <div className="circle_for_design"></div>
-          <div className="second_circle"></div>
         </div>
-        <div className="nextToMiddle">
-          <div className="images">
-            <div className="first">
-              <img src="#" alt="img" />
-            </div>
-            <div className="second">
-              <img src="#" alt="img" />
-            </div>
-            <div className="third">
-              <img src="#" alt="img" />
-            </div>
-            <div className="fiveth">
-              <img src="#" alt="img" />
-            </div>
-            <div className="sixth">
-              <img src="#" alt="img" />
-            </div>
-            <div className="seventh">
-              <img src="#" alt="img" />
-            </div>
+        <div className="photo-photo">
+        <div className="kanteiner-photobox">
+            <img src={perspectivephoto2} alt="perspectivephoto2" className="perspectivephoto2"/>
+            <img src={perspectivephoto3} alt="perspectivephoto3" className="perspectivephoto2"/>
           </div>
         </div>
       </div>
