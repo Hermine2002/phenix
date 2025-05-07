4a91009cedacb6f125bcb1665aa2ebb9997ea547
itfabersllc <126962107+itfabersllc@users.noreply.github.com>
2025-05-07 11:50:50 -0700

    lang

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
 import "./App.css";
 import Routers from "./Routes/Routes";
 import MouseFollower from "./components/MouseFollower";
+import {useTranslation, I18nextProvider} from 'react-i18next';
+import i18next from 'i18next';
+import enTranslation from './locales/en.json';
+import ruTranslation from './locales/ru.json';
+import amTranslation from './locales/am.json';
+
+i18next.init({
+    lng: 'en',
+    resources: {
+        en: {
+            translation: enTranslation
+        },
+        ru: {
+            translation: ruTranslation
+        },
+        am: {
+            translation: amTranslation
+        }
+    }
+});
+
 function App() {
-  return (
-    <>
-      <MouseFollower />
-      <Routers />
-    </>
-  );
+    return (
+        <I18nextProvider i18n={i18next}>
+            <MouseFollower/>
+            <Routers/>
+        </I18nextProvider>
+    );
 }
 
 export default App;
